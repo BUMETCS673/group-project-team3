9a481dfecbf5f869f277dff66f7eba15e3995d2f
JingTian2022 <105386475+JingTian2022@users.noreply.github.com>
2022-06-11 10:08:21 -0400

    modified configuration, now app will use local api urls instead of hardcode ones

--- stock_getter/backend_stockgetter/stock_service/views.py
+++ stock_getter/backend_stockgetter/stock_service/views.py
@@ -25,13 +25,11 @@ class StockListServiceAPIView(generics.ListCreateAPIView):
     """
     def __init__(self, **kwargs: Any) -> None:
         super().__init__(**kwargs)
-        # Parse API URL and key and intitialize
-        # config = configparser.ConfigParser()
-        # config.read('api_keys.ini')
-        # self.stock_list_url = config['APIURLS']['stock_list']
-
-        #TODO: Fix this to read config files
-        self.stock_list_url = 'https://fcsapi.com/api-v3/stock/list?country=United-states&access_key=4wcNF3TiJ7XVx6p8zDt21' 
+        
+        # Parse API URL and key from local configuration file
+        config = configparser.ConfigParser()
+        config.read('api_keys.ini')
+        self.stock_list_url = config['API_URLS']['stock_list']
 
     def get(self, request, *args, **kwargs):
         """This function handles GET requests
@@ -75,16 +73,13 @@ class StockDataServiceAPIView(generics.ListCreateAPIView):
     """
     def __init__(self, **kwargs: Any) -> None:
         super().__init__(**kwargs)
-        # config = configparser.ConfigParser()
-        # config.read('api_keys.ini')
-        # self.stock_data_daily_url = config['APIURLS']['stock_data_daily']
-        # self.stock_data_weekly_url = config['APIURLS']['stock_data_weekly']
-        # self.stock_data_monthly_url = config['APIURLS']['stock_data_monthly']
 
-        #TODO: Fix this to read config files
-        self.stock_data_daily_url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&outputsize=full&apikey=Q5SUUT82ASKLSWB1'
-        self.stock_data_weekly_url = 'https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&apikey=Q5SUUT82ASKLSWB1'
-        self.stock_data_monthly_url = 'https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&apikey=Q5SUUT82ASKLSWB1'
+        # Parse API URL and key from local configuration file
+        config = configparser.ConfigParser()
+        config.read('api_keys.ini')
+        self.stock_data_daily_url = config['API_URLS']['stock_data_daily']
+        self.stock_data_weekly_url = config['API_URLS']['stock_data_weekly']
+        self.stock_data_monthly_url = config['API_URLS']['stock_data_monthly']
 
     def get(self, request, *args, **kwargs):
         """This function handles GET requests. Not implemented for now.
